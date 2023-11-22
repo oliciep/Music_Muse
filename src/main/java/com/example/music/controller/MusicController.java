605e8447426c8b3060e1bdf14941eb3924673fb6
Oliver Cieplinski <olivercieplinski@gmail.com>
2023-11-22 23:51:26 +0000

    Added React Frontend

--- src/main/java/com/example/music/controller/MusicController.java
+++ src/main/java/com/example/music/controller/MusicController.java
@@ -1,14 +1,14 @@
 package com.example.music.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MusicController {
 
-	@GetMapping("/")
+	@RequestMapping(value = "/")
 	public String index() {
-		return "Welcome to my Music Application!";
+		return "forward:/index.html";
 	}
 
 }
