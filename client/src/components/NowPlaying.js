08b3f73212bf18f78aa1ba681d75c494f3f8088f
Oliver Cieplinski <olivercieplinski@gmail.com>
2024-04-25 00:19:46 +0100

    Initial components created

--- client/src/components/NowPlaying.js
+++ client/src/components/NowPlaying.js
@@ -1,46 +1,47 @@
 // components/NowPlaying.js
 import React from "react";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 
 const NowPlaying = ({ nowPlaying }) => {
   return (
-    <div style={{ marginBottom: "20px" }}>
-      <Typography variant="h2" color="primary" gutterBottom>
-        Your Recent Tracks
-      </Typography>
-      <div
-        style={{
-          backgroundColor: "lightgreen",
-          display: "flex",
-          alignItems: "center",
-          marginBottom: "10px",
-        }}
-      >
+    <Box
+      sx={{
+        backgroundColor: "lightgreen",
+        borderRadius: "5px",
+        padding: "10px",
+        marginBottom: "20px",
+      }}
+    >
+      <Box sx={{ display: "flex", alignItems: "center" }}>
         {nowPlaying.albumArt && (
-          <img
+          <Box
+            component="img"
             src={nowPlaying.albumArt}
-            alt="Track Album"
-            style={{ width: "50px", height: "50px", marginRight: "10px" }}
+            alt="Now Playing Album Art"
+            sx={{ width: "50px", height: "50px", marginRight: "10px" }}
           />
         )}
-        <Typography variant="h5" color="primary" style={{ flex: 1 }}>
+        <Typography variant="h5" color="primary" sx={{ minWidth: "220px" }}>
           <a href={nowPlaying.url} target="_blank" rel="noopener noreferrer">
-            {nowPlaying.name}
+            <b>{nowPlaying.name}</b>
           </a>
         </Typography>
-        <Typography variant="h5" color="primary" style={{ flex: 1 }}>
+        <Typography variant="h5" color="primary" sx={{ marginLeft: "auto" }}>
           <a
             href={nowPlaying.artistUrl}
             target="_blank"
             rel="noopener noreferrer"
           >
-            {nowPlaying.artist}
+            <b>{nowPlaying.artist}</b>
           </a>
         </Typography>
-        {nowPlaying.albumArt && <VolumeUpIcon style={{ color: "darkgreen" }} />}
-      </div>
-    </div>
+        {nowPlaying.albumArt && (
+          <VolumeUpIcon sx={{ color: "darkgreen", marginLeft: "10px" }} />
+        )}
+      </Box>
+    </Box>
   );
 };
 
--- client/src/components/RecentTracks.js
+++ client/src/components/RecentTracks.js
@@ -14,35 +14,28 @@ const RecentTracks = ({ recentTracks }) => {
       {recentTracks.map((track, index) => (
         <Box
           key={index}
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            marginBottom: "10px",
-          }}
+          sx={{ display: "flex", alignItems: "center", marginBottom: "10px" }}
         >
-          <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Box
+            sx={{ display: "flex", alignItems: "center", minWidth: "220px" }}
+          >
             {track.image && (
               <Box
                 component="img"
                 src={track.image}
                 alt={track.name}
-                sx={{ width: "50px", height: "50px", marginRight: "10px" }}
+                sx={{ width: "50px", height: "50px", marginRight: "20px" }}
               />
             )}
-            <Typography
-              variant="h5"
-              color="primary"
-              sx={{ flexGrow: 1, marginRight: "10px" }}
-            >
+            <Typography variant="h5" color="primary">
               <a href={track.url} target="_blank" rel="noopener noreferrer">
-                {track.name}
+                <b>{track.name}</b>
               </a>
             </Typography>
           </Box>
-          <Typography variant="h5" color="primary" sx={{ flexGrow: 1 }}>
+          <Typography variant="h5" color="primary" sx={{ marginLeft: "auto" }}>
             <a href={track.artistUrl} target="_blank" rel="noopener noreferrer">
-              {track.artist}
+              <b>{track.artist}</b>
             </a>
           </Typography>
         </Box>
